7ce16e6cbedfc08c7b74a4d176425b83e8350a23
MarisolOrtiz <64797672+MarisolOrtiz@users.noreply.github.com>
2020-05-16 10:04:00 -0600

    proyecto_final

---
+++ src/ultimo4y5/Marisol.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimo4y5;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Marisol {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("ejercios de tactico ");
+        System.out.println("alumna BERTHA   sis22A  carnet 189619 ");
+        System.out.println("VIVO en libertada puerto de la libertad  "); 
+    } 
+}
